f7207576b0fef5483956d137312b5e952bf3825b
Wbit504 <keegswallace@gmail.com>
2020-09-13 01:39:13 +1200

    Commit 2

--- git/src/git/Main.java
+++ git/src/git/Main.java
@@ -2,7 +2,8 @@ package git;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.print("Hi, my name is Keegan"); 
+		System.out.println("Hi, my name is Keegan"); 
+		System.out.println("My favourite color is blue ");
 		
 	}
 }
